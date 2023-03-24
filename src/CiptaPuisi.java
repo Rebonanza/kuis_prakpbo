dc93131906c39e38ed1f7ff314530c391979ae6f
Rebonanza <giventheo2211@gmail.com>
2023-03-24 09:33:07 +0700

    initial commmit

---
+++ src/CiptaPuisi.java
@@ -0,0 +1,87 @@
+import javax.swing.*;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.font.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CiptaPuisi extends JFrame {
+    // INSTANSIASI OBJEK KOMPONEN GUI
+
+    JLabel lJudul = new JLabel("Penilaian Cipta Puisi");
+    JLabel lBiodata = new JLabel("Form Biodata");
+    JLabel lPenilaian = new JLabel("Form Penilaian (0-100)");
+    JPanel PanelBiodata = new JPanel();
+    JPanel PanelPenialian = new JPanel();
+
+    JLabel lNama = new JLabel("Nama");
+    JLabel lJurusan = new JLabel("Jurusan");
+    JLabel lNim = new JLabel("NIM");
+    JTextField fNama = new JTextField();
+    JTextField fJurusan = new JTextField();
+    JTextField fNim = new JTextField();
+
+    JLabel lBahasa = new JLabel("Tata Bahasa");
+    JLabel lEYD = new JLabel("EYD");
+    JLabel lStruktur = new JLabel("Struktur");
+    JLabel lKreativitas = new JLabel("Kreativitas");
+    JTextField fBahasa = new JTextField();
+    JTextField fEYD = new JTextField();
+    JTextField fStruktur = new JTextField();
+    JTextField fKreativitas = new JTextField();
+
+    JButton btnSimpan = new JButton("Simpan");
+    JButton btnBatal = new JButton("Batal");
+    // JLabel lUsername = new JLabel("Username");
+    // JLabel lPassword = new JLabel("Password");
+
+    // JTextField fJudul = new JTextField();
+    // JTextField fUsername = new JTextField();
+    // JPasswordField fPassword = new JPasswordField();
+
+    public CiptaPuisi() {
+        setTitle("Form Penilaian Cipta Puisi");
+        setLayout(null);
+        setSize(1200, 1000);
+
+        PanelBiodata.add(lNama);
+        PanelBiodata.add(lJurusan);
+        PanelBiodata.add(lNim);
+        PanelBiodata.add(fNama);
+        PanelBiodata.add(fJurusan);
+        PanelBiodata.add(fNim);
+
+        PanelPenialian.add(lBahasa);
+        PanelPenialian.add(lEYD);
+        PanelPenialian.add(lKreativitas);
+        PanelPenialian.add(lStruktur);
+        PanelPenialian.add(fBahasa);
+        PanelPenialian.add(fEYD);
+        PanelPenialian.add(fKreativitas);
+        PanelPenialian.add(fStruktur);
+
+        add(lJudul);
+        add(lBiodata);
+        add(lPenilaian);
+        add(btnSimpan);
+        add(btnBatal);
+        add(PanelBiodata);
+        add(PanelPenialian);
+
+        lJudul.setBounds(100, 30, 200, 50);
+        lJudul.setFont(new Font("Times New Roman", Font.BOLD, 18));
+        lBiodata.setBounds(50, 80, 200, 50);
+        lBiodata.setFont(new Font("Arial", Font.PLAIN, 16));
+        PanelBiodata.setBounds(50, 120, 300, 600);
+        lPenilaian.setBounds(50, 60, 400, 50);
+        lPenilaian.setFont(new Font("Arial", Font.PLAIN, 16));
+        PanelBiodata.setBounds(50, 500, 300, 600);
+        btnSimpan.setBounds(140, 400, 150, 50);
+        btnBatal.setBounds(200, 400, 150, 50);
+        this.setSize(400, 500);
+        this.setVisible(true);
+    }
+}
---
+++ src/HomePage.java
@@ -0,0 +1,226 @@
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.font.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class HomePage extends JFrame {
+    // INSTANSIASI OBJEK KOMPONEN GUI
+    JLabel lJudul = new JLabel("Aplikasi Penilaian Lomba Karya Tulis dan Cipta Puisi");
+    JLabel lUniv = new JLabel("UPN \"Veteran\" Yogyakarta");
+    JButton btnKartul = new JButton("Karya Tulis");
+    JButton btnPuisi = new JButton("Cipta Puisi");
+    JButton btnClose = new JButton("Close");
+    // JLabel lUsername = new JLabel("Username");
+    // JLabel lPassword = new JLabel("Password");
+
+    // JTextField fJudul = new JTextField();
+    // JTextField fUsername = new JTextField();
+    // JPasswordField fPassword = new JPasswordField();
+
+    // JTable table;
+    // JScrollPane scrollPane;
+    // Object namaKolom[] = {"Kode", "Judul", "Jumlah"};
+    // String query, dataPilih, kodeLama;
+    // String data[][];
+    // String dbTabel = "";
+    // Connect koneksi = new Connect(); //cek koneksi
+
+    public HomePage() {
+        setTitle("Aplikasi Penilaian Lomba Karya Tulis dan Cipta Puisi");
+        setLayout(null);
+        setSize(1200, 1000);
+        add(lJudul);
+        add(lUniv);
+        add(btnKartul);
+        add(btnPuisi);
+        add(btnClose);
+
+        lJudul.setBounds(100, 30, 600, 50);
+        lJudul.setFont(new Font("Times New Roman", Font.BOLD, 24));
+        lUniv.setBounds(300, 60, 600, 50);
+        lUniv.setFont(new Font("Times New Roman", Font.ITALIC, 16));
+        btnKartul.setBounds(200, 160, 150, 50);
+        btnPuisi.setBounds(400, 160, 150, 50);
+        btnClose.setBounds(650, 300, 80, 30);
+        this.setSize(800, 400);
+        this.setVisible(true);
+    }
+
+    // btnLogin.addActionListener(new ActionListener() {
+    // @Override
+    // public void actionPerformed(ActionEvent e) {
+    // // try {
+    // // String query = "insert into " + dbTabel + " values ('" + getKode() +
+    // // "','" + getJudul() + "','" + getJumlah() +"')";
+    // // koneksi.statement = koneksi.connect.createStatement();
+    // // koneksi.statement.executeUpdate(query);
+    // // JOptionPane.showMessageDialog(null, "Insert Data Berhasil!");
+    // // } catch (Exception exception){
+    // // JOptionPane.showMessageDialog(null, "Insert Data Gagal!");
+    // // } finally {
+    // // updateData();
+    // // }
+    // }
+    // });
+}
+// public String getUsername(){
+// return fUsername.getText();
+// }
+
+// public String getJudul(){
+// return fJudul.getText();
+// }
+
+// public int getPassword(){
+// return fPassword.getText();
+// }
+
+// if (getBanyakData() != 0){
+// updateData();
+// }
+
+// DefaultTableModel model = new DefaultTableModel(data, namaKolom);
+// table.setModel(model);
+
+// table.addMouseListener(new MouseAdapter() {
+// @Override
+// public void mouseClicked(MouseEvent e) {
+// super.mouseClicked(e);
+// try {
+// int baris = table.getSelectedRow();
+// dataPilih = table.getValueAt(baris, 0).toString();
+// System.out.println(dataPilih);
+// if (dataPilih != null){
+// String pilih;
+// kodeLama = table.getValueAt(baris, 0).toString();
+// fKode.setText(kodeLama);
+// pilih = table.getValueAt(baris, 1).toString();
+// fJudul.setText(pilih);
+// pilih = table.getValueAt(baris, 2).toString();
+// fJumlah.setText(pilih);
+
+// btnUbah.setEnabled(true);
+// btnHapus.setEnabled(true);
+// btnSimpan.setEnabled(false);
+// }
+// } catch (Exception e1){
+// System.out.println(e1.getMessage());
+// System.out.println("Error Mouse");
+// }
+// }
+// });
+
+// btnRegister.addActionListener(new ActionListener() {
+// @Override
+// public void actionPerformed(ActionEvent e) {
+// // try {
+// // String kodeBaru = getKode();
+// // String judulBaru = getJudul();
+// // int jumlahBaru = getJumlah();
+
+// // // query = "update `"+dbTabel+"` set `kode` = '"+kodeBaru+"', `judul` =
+// '"+
+// // // judulBaru+"', `jumlah` = '"+jumlahBaru+"' where `kode` = '"+
+// // // kodeLama+"'";
+// // koneksi.statement = koneksi.connect.createStatement();
+// // koneksi.statement.executeUpdate(query);
+// // JOptionPane.showMessageDialog(null, "Update data berhasil!");
+// // } catch (Exception e1){
+// // JOptionPane.showMessageDialog(null, "Update data gagal!");
+// // } finally {
+// // updateData();
+// // dataPilih="";
+// // btnUbah.setEnabled(false);
+// // btnHapus.setEnabled(false);
+// // btnSimpan.setEnabled(true);
+// // }
+// // }
+// });
+
+// // btnHapus.addActionListener(new ActionListener() {
+// // @Override
+// // public void actionPerformed(ActionEvent e) {
+// // try {
+// // query = "delete from `"+dbTabel+"` where `kode` = '"+kodeLama+"'";
+// // koneksi.statement = koneksi.konek.createStatement();
+// // koneksi.statement.executeUpdate(query);
+// // JOptionPane.showMessageDialog(null, "Hapus data berhasil!");
+// // } catch (Exception e1){
+// // JOptionPane.showMessageDialog(null, "Hapus data gagal!");
+// // } finally {
+// // updateData();
+// // btnUbah.setEnabled(false);
+// // btnHapus.setEnabled(false);
+// // btnSimpan.setEnabled(true);
+// // }
+// // }
+// // });
+
+// setVisible(true);
+// setLocationRelativeTo(null);
+// setDefaultCloseOperation(EXIT_ON_CLOSE);
+// }
+
+// // hitung jumlah data
+// public int getBanyakData(){
+// int jumlah = 0;
+// try{
+// koneksi.statement = koneksi.konek.createStatement();
+// query = "Select * from " + dbTabel;
+// ResultSet resultSet = koneksi.statement.executeQuery(query);
+// while (resultSet.next()){
+// jumlah++;
+// }
+// return jumlah;
+// } catch (SQLException e){
+// System.out.println(e.getMessage());
+// System.out.println("SQL Error!");
+// return 0;
+// }
+// }
+
+// public void updateData(){
+// data = readData();
+// // table.setModel((new JTable(data, namaKolom)).getModel());
+
+// fJudul.setText("");
+// fKode.setText("");
+// fJumlah.setText("");
+
+// table = new JTable(data, namaKolom);
+// scrollPane = new JScrollPane(table);
+// add(scrollPane);
+// scrollPane.setBounds(20, 200, 600, 300);
+// scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
+// }
+
+// // Mengambil data dari database
+// public String[][] readData(){
+// int jumlah = 0;
+// try{
+// String data[][] = new String[getBanyakData()][3];
+// query = "Select * from "+dbTabel;
+// ResultSet resultSet = koneksi.statement.executeQuery(query);
+// while (resultSet.next()){
+// data[jumlah][0] = String.valueOf(resultSet.getString("kode"));
+// data[jumlah][1] = resultSet.getString("judul");
+// data[jumlah][2] = String.valueOf(resultSet.getInt("jumlah"));
+// jumlah++;
+// }
+// return data;
+// } catch (SQLException e){
+// System.out.println(e.getMessage());
+// System.out.println("SQL Error Read Data!");
+// return null;
+// }
+// }
+
+// }
